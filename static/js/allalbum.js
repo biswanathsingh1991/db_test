e84fe8ef2e9ab03e5abc879964e84021a13b9865
biswanath <biswanathsingh1991@gmail.com>
2018-08-21 18:56:00 +0530

    21082018

--- static/js/allalbum.js
+++ static/js/allalbum.js
@@ -3,12 +3,12 @@ Vue.component('test12', {
   props: ['title'],
   data: function () {
     return {
-      count:0,
+      
 
     }
   },
     template: '<div> <ul><li v-for="title in title">{{ title.title }}<li></ul></div>'
-}),
+});
 
 
 
@@ -16,18 +16,25 @@ var allalbum = new Vue({
 
   el: '#allalbum',
   delimiters: ['[[', ']]'],
-  data:{
+  data : {
+    info: null,
+    joy : "name-joy"
   },
-  mounted (){
-    $ajax({
-      url : '/allalbumapi/',
-      dataType: "json",
-      type: "GET",
-      sucess: function(data){
-        console.log(data)
-        this.obj = data
-      },
-    }),
-  } ,
+    mounted (){
+      // $.ajax({
+      //   url : '/allalbumapi/',
+      //   dataType : 'json',
+      //   type : 'GET',
+      //   success : function(data){
+      //     this.obj = data;
+      //     // this.obj.valueset = true ;
+      //   }
+      //
+      // })
 
-})
+      axios
+     .get('/allalbumapi/')
+     .then(response => (this.info = response))
+  },
+
+});
